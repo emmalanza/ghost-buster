6729b979c7cadeaf498ce043a048b2ea19e02015
emmalanza <emmalanza@hotmail.com>
2025-02-04 19:23:41 +0100

    setting up GhosBustTest class

--- src/test/java/com/f5/ghostbuster/models/GhostBusterTest.java
+++ src/test/java/com/f5/ghostbuster/models/GhostBusterTest.java
@@ -1,6 +1,19 @@
 package com.f5.ghostbuster.models;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class GhostBusterTest {
-    
+    private GhostBuster ghostBuster;
+    @BeforeEach
+    public void setUp() {
+        ghostBuster = new GhostBuster();
+        Ghost ghost1 = new Ghost("Espíritu del Pescador de Lastres", Ghost.Class.IV, Ghost.DangerLevel.ALTO, "Aparecer durante tormentas en la costa");
+        Ghost ghost2 = new Ghost("Espíritu del Río Caudal", Ghost.Class.V, Ghost.DangerLevel.CRITICO, "Aparecer durante tormentas en la costa");
+        Ghost ghost3 = new Ghost("Espíritu de Turón", Ghost.Class.I, Ghost.DangerLevel.BAJO, "Aparecer durante tormentas en la costa");
+        ghostBuster.captureGhost(ghost1);
+        ghostBuster.captureGhost(ghost2);
+        ghostBuster.captureGhost(ghost3);   
+    }
+
     
 }
