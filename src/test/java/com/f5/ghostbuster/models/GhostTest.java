44704c5726a25f07eed29b21fa97145439f74671
unknown <gaviriayeferson2@gmail.com>
2025-02-04 12:16:24 +0100

    the testInitialCorrectlyGhostName is approved

--- src/test/java/com/f5/ghostbuster/models/GhostTest.java
+++ src/test/java/com/f5/ghostbuster/models/GhostTest.java
@@ -3,6 +3,7 @@ package com.f5.ghostbuster.models;
 import org.junit.jupiter.api.DisplayName;
 import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.is;
 
 public class GhostTest {
 
@@ -12,7 +13,7 @@ public class GhostTest {
         
         //Given
         String name = "Espíritu del Pescador de Lastres";
-        Ghost ghost = new Ghost(name, Ghost.Class.IV,Ghost.DangerLevel.HIGH, "Aparecer durante tormentas en la costa");
+        Ghost ghost = new Ghost(name, Ghost.Class.IV, Ghost.DangerLevel.ALTO, "Aparecer durante tormentas en la costa");
 
         //Then
         assertThat(ghost.getName(), is(name));
