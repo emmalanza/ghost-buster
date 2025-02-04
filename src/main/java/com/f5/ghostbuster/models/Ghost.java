91b38171685b112cef5c39e5a3a31d3717543b2d
unknown <gaviriayeferson2@gmail.com>
2025-02-04 12:11:26 +0100

    add the attributes , constructor and getname method to the Ghost class to pass test

--- src/main/java/com/f5/ghostbuster/models/Ghost.java
+++ src/main/java/com/f5/ghostbuster/models/Ghost.java
@@ -2,4 +2,21 @@ package com.f5.ghostbuster.models;
 
 public class Ghost {
 
+    private String name;
+
+    public Ghost(String name, Ghost.Class ghostClass, Ghost.DangerLevel dangerLevel, String hability) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+   public enum Class {
+        I, II, III, IV, V
+    }
+
+    public enum DangerLevel {
+        BAJO, MEDIO, ALTO, CRITICO
+    }
 }
